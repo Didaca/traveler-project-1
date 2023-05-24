21df4f08f330123cf2446dccef239e6c3841db56
Didaca <deni_sk8@abv.bg>
2023-05-24 18:43:55 +0300

    Update css testimonials

--- src/components/Testimonials/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials/Testimonials.js
@@ -53,8 +53,7 @@ export const Testimonials = () => {
                             <img className={styles.img} src="/images/testimonial-3.jpg" alt="Owner" />
                             <div className={styles.testimonials_info_content_text}>
                                 <p className={styles.testimonial_p}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu</p>
-                                <h5 className={styles.testimonials_info_name}>Konstantin</h5>
-                                <h5 className={styles.testimonials_info_name}>Konstantinov</h5>
+                                <h5 className={styles.testimonials_info_name}>Konstantin Konstantinov</h5>
                             </div>
                         </div>
                     </article>
@@ -63,8 +62,7 @@ export const Testimonials = () => {
                             <img className={styles.img} src="/images/testimonial-2.jpg" alt="Owner" />
                             <div className={styles.testimonials_info_content_text}>
                                 <p className={styles.testimonial_p}>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.</p>
-                                <h5 className={styles.testimonials_info_name}>Ivelina</h5>
-                                <h5 className={styles.testimonials_info_name}>Ivanova</h5>
+                                <h5 className={styles.testimonials_info_name}>Ivelina Ivanova</h5>
                             </div>
                         </div>
                     </article>
@@ -73,8 +71,7 @@ export const Testimonials = () => {
                             <img className={styles.img} src="/images/testimonial-1.jpg" alt="Owner" />
                             <div className={styles.testimonials_info_content_text}>
                                 <p className={styles.testimonial_p}>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the gre</p>
-                                <h5 className={styles.testimonials_info_name}>Georgi</h5>
-                                <h5 className={styles.testimonials_info_name}>Georgiev</h5>
+                                <h5 className={styles.testimonials_info_name}>Georgi Georgiev</h5>
                             </div>
                         </div>
                     </article>
